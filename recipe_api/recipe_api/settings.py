3ceb816eafa5849b6d21102727e571b290329b9a
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-09-07 15:34:06 -0700

    Ensuring all static files are set for deployment

--- recipe_api/recipe_api/settings.py
+++ recipe_api/recipe_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os #added for deployment
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0wew$&_f7yxn=m&$s@%u7-c%kamtyvaor3nyj17hu7=wml9xnx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #change to True for development
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swiss003.pythonanywhere.com']
 
 
 from datetime import timedelta
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
