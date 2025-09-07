3b2d94694f3f441988e42c17dd9b6e2c8ce3f004
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-09-07 16:44:46 -0700

    WSGI file config.

--- recipe_api/recipe_api/wsgi.py
+++ recipe_api/recipe_api/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Path to the project root
+project_home = '/home/Swiss003/Recipe_Management_API/recipe_api'
+if project_home not in sys.path:
+    sys.path.append(project_home)
+
+# Set the Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'recipe_api.recipe_api.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_api.recipe_api.settings')
-
 application = get_wsgi_application()
